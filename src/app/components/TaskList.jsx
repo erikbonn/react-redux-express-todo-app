dfeeca353b1c42eb6151f066f7e631f622efff8e
Erik Bonn <erik.bonn@biofiredx.com>
2019-05-28 17:33:03 -0600

    Actions and reducers added to show changes in state

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { requestTaskCreation } from '../store/mutations';
+import { Link } from 'react-router-dom'
 
-export const TaskList = ({tasks, name}) => (
+export const TaskList = ({ tasks, name, id, createNewTask }) => (
   <div>
     <h3>
       {name}
     </h3>
-    {tasks.map(task=>(<div key={task.id}>{task.name}</div>))}
+    <div>
+      {tasks.map(task => (
+        <Link to={`/task/${task.id}`} key={task.id}>
+        <div>{task.name}</div>
+        </Link>))}
     </div>
+    <button onClick={() => createNewTask(id)}>Add New</button>
+  </div>
 )
 
 const mapStateToProps = (state, ownProps) => {
@@ -15,8 +23,16 @@ const mapStateToProps = (state, ownProps) => {
   return {
     name: ownProps.name,
     id: groupID,
-    tasks: state.tasks.filter(task=>task.group === groupID)
+    tasks: state.tasks.filter(task => task.group === groupID)
   }
 }
 
-export const ConnectedTaskList = connect(mapStateToProps)(TaskList)
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    createNewTask (id) {
+      console.log("Creating new task...", id)
+      dispatch(requestTaskCreation(id))
+    }
+  }
+}
+export const ConnectedTaskList = connect(mapStateToProps, mapDispatchToProps)(TaskList)
